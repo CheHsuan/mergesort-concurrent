c4d2e78cfb4502e93c9c1f8989554693fec7ef02
CheHsuan <exas60807@gmail.com>
2016-10-13 15:07:57 +0800

    add benchmark

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ OBJS = list.o threadpool.o
 
 GIT_HOOKS := .git/hooks/pre-commit
 
-all: $(GIT_HOOKS) sort sort_autotest
+all: $(GIT_HOOKS) sort sort_bench
 
 $(GIT_HOOKS):
 	@scripts/install-git-hooks
@@ -20,11 +20,24 @@ deps := $(OBJS:%.o=.%.o.d)
 sort: $(OBJS) main.c mergesort.c
 	$(CC) $(CFLAGS) -o $@ $^ -rdynamic
 
-sort_autotest: $(OBJS) main.c mergesort.c
+sort_bench: $(OBJS) main.c mergesort.c
 	$(CC) $(CFLAGS) -DBENCH -o $@ $^ -rdynamic
 
+nums=100000
+gencsv: sort_bench
+	printf ",2-threads,4-threads,8-threads,16-threads\n" > result_clock_gettime.csv
+	for i in `seq 1 1 100`; do \
+	    (for j in {1..$(nums)}; do echo $$RANDOM; done > numbers.txt); \
+	    printf "%d," $$i >> result_clock_gettime.csv; \
+	    ./sort_bench 2 $(nums) < numbers.txt >> result_clock_gettime.csv; \
+	    ./sort_bench 4 $(nums) < numbers.txt >> result_clock_gettime.csv; \
+	    ./sort_bench 8 $(nums) < numbers.txt >> result_clock_gettime.csv; \
+	    ./sort_bench 16 $(nums) < numbers.txt >> result_clock_gettime.csv; \
+	    printf "\n" >> result_clock_gettime.csv; \
+	done
+
 clean:
-	rm -f *.o sort sort_autotest
+	rm -f *.o sort sort_bench result_clock_gettime.csv numbers.txt
 	@rm -rf $(deps)
 
 -include $(deps)
--- main.c
+++ main.c
@@ -8,6 +8,10 @@
 
 #define USAGE "usage: ./sort [thread_count] [input_count]\n"
 
+#ifdef BENCH
+#define CLOCK_ID CLOCK_REALTIME
+#endif
+
 struct {
     pthread_mutex_t mutex;
     int cut_thread_count;
@@ -18,6 +22,19 @@ llist_t *the_list = NULL;
 
 int thread_count = 0, data_count = 0, max_cut = 0;
 tpool_t *pool = NULL;
+
+static double diff_in_second(struct timespec t1, struct timespec t2)
+{
+    struct timespec diff;
+    if (t2.tv_nsec-t1.tv_nsec < 0) {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec - 1;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + 1000000000;
+    } else {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
+    }
+    return (diff.tv_sec + diff.tv_nsec / 1000000000.0);
+}
 
 static void *task_run(void *data)
 {
@@ -43,6 +60,9 @@ int main(int argc, char const *argv[])
         printf(USAGE);
         return -1;
     }
+#ifdef BENCH
+    struct timespec start, end;
+#endif
     thread_count = atoi(argv[1]);
     data_count = atoi(argv[2]);
     max_cut = thread_count * (thread_count <= data_count) +
@@ -67,13 +87,20 @@ int main(int argc, char const *argv[])
     pool = (tpool_t *) malloc(sizeof(tpool_t));
     tpool_init(pool, thread_count, task_run);
 
+#ifdef BENCH
+    clock_gettime(CLOCK_ID, &start);
+#endif
     /* launch the first task */
     task_t *_task = (task_t *) malloc(sizeof(task_t));
     _task->func = cut_func;
     _task->arg = the_list;
     tqueue_push(pool->queue, _task);
-
     /* release thread pool */
     tpool_free(pool);
+#ifdef BENCH
+    clock_gettime(CLOCK_ID, &end);
+    printf("%lf,", diff_in_second(start, end));
+#endif
+
     return 0;
 }
